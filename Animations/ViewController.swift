67cfc5a88f2c11104fe356176fa2717e02c503cf
kenwilcox <wilcoxk@gmail.com>
2014-12-21 22:59:36 -0700

    damping - this is kinda neat

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -24,11 +24,21 @@ class ViewController: UIViewController {
       let newFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
       coloredSquare.frame = CGRect(x: self.view.frame.width - newFrame.width, y:120, width: newFrame.width, height: newFrame.height)
       }, completion: { finished in
-        UIView.animateWithDuration(1.0, animations: {
+        UIView.animateWithDuration(1.0, delay: 1.0, options: .CurveEaseOut, animations: {
           coloredSquare.backgroundColor = UIColor.blueColor()
           coloredSquare.frame = CGRect(x: 0, y: 120, width: 50, height: 50)
+          }, completion: {finished in
+        
+            UIView.animateWithDuration(1.0, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: .CurveEaseInOut, animations: {
+              coloredSquare.backgroundColor = UIColor.greenColor()
+              coloredSquare.frame = CGRect(x: 0, y: self.view.frame.height - 100, width: 100, height: 100)
+              }, completion: { finished in
+                
+            })
+            
         })
     })
+
   }
 
   override func didReceiveMemoryWarning() {

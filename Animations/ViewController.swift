3b69abff9de417534641a504bb3990e54ccd4cd2
kenwilcox <wilcoxk@gmail.com>
2014-12-25 16:47:39 -0700

    Added some randomness

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -17,28 +17,31 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     let coloredSquare = UIView()
+    let size : CGFloat = CGFloat( Int(rand()) %  40 + 20)
+    let yPosition : CGFloat = CGFloat( Int(rand()) %  200 + 20)
+    
     coloredSquare.backgroundColor = UIColor.blueColor()
-    coloredSquare.frame = CGRect(x: 0, y: 120, width: 50, height: 50)
+    coloredSquare.frame = CGRect(x: 0, y: yPosition, width: size, height: size)
     
     self.view.addSubview(coloredSquare)
     
     UIView.animateWithDuration(1.0, animations: {
       coloredSquare.backgroundColor = UIColor.redColor()
-      let newFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
-      coloredSquare.frame = CGRect(x: self.view.frame.width - newFrame.width, y:120, width: newFrame.width, height: newFrame.height)
+      let newFrame = CGRect(x: 0, y: 0, width: size * 2, height: size * 2)
+      coloredSquare.frame = CGRect(x: self.view.frame.width - newFrame.width, y:yPosition, width: newFrame.width, height: newFrame.height)
       }, completion: { finished in
         UIView.animateWithDuration(1.0, delay: 1.0, options: .CurveEaseOut, animations: {
           coloredSquare.backgroundColor = UIColor.blueColor()
-          coloredSquare.frame = CGRect(x: 0, y: 120, width: 50, height: 50)
+          coloredSquare.frame = CGRect(x: 0, y: yPosition, width: size, height: size)
           }, completion: {finished in
             
             UIView.animateWithDuration(1.0, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: .CurveEaseInOut | .Autoreverse, animations: {
               coloredSquare.backgroundColor = UIColor.greenColor()
-              coloredSquare.frame = CGRect(x: 0, y: self.view.frame.height - 100, width: 100, height: 100)
+              coloredSquare.frame = CGRect(x: 0, y: self.view.frame.height - (size * 2), width: size * 2, height: size * 2)
               }, completion: { finished in
                 // Go back to "home"
                 coloredSquare.backgroundColor = UIColor.blueColor()
-                coloredSquare.frame = CGRect(x: 0, y: 120, width: 50, height: 50)
+                coloredSquare.frame = CGRect(x: 0, y: yPosition, width: size, height: size)
                 coloredSquare.removeFromSuperview()
             })
             

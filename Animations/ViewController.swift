15e398bdc6ab6866fe146363be0cd57543cb6c39
kenwilcox <wilcoxk@gmail.com>
2014-12-25 16:41:22 -0700

    Added a button, moved the viewDidLoad to buttonPressed

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
+  }
+
+  @IBAction func animateButtonPressed(sender: UIButton) {
     super.viewDidLoad()
     
     let coloredSquare = UIView()
@@ -28,7 +31,7 @@ class ViewController: UIViewController {
           coloredSquare.backgroundColor = UIColor.blueColor()
           coloredSquare.frame = CGRect(x: 0, y: 120, width: 50, height: 50)
           }, completion: {finished in
-        
+            
             UIView.animateWithDuration(1.0, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: .CurveEaseInOut | .Autoreverse, animations: {
               coloredSquare.backgroundColor = UIColor.greenColor()
               coloredSquare.frame = CGRect(x: 0, y: self.view.frame.height - 100, width: 100, height: 100)
@@ -36,13 +39,13 @@ class ViewController: UIViewController {
                 // Go back to "home"
                 coloredSquare.backgroundColor = UIColor.blueColor()
                 coloredSquare.frame = CGRect(x: 0, y: 120, width: 50, height: 50)
+                coloredSquare.removeFromSuperview()
             })
             
         })
     })
-
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.

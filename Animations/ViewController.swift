78ab1bce3d08961ea1408dbd5a38b5de1c3a53f3
kenwilcox <wilcoxk@gmail.com>
2014-12-25 17:05:11 -0700

    Added a random delay, start off screen, default to 10 squares

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -15,37 +15,41 @@ class ViewController: UIViewController {
   }
 
   @IBAction func animateButtonPressed(sender: UIButton) {
-    let coloredSquare = UIView()
-    let size : CGFloat = CGFloat( Int(rand()) %  40 + 20)
-    let yPosition : CGFloat = CGFloat( Int(rand()) %  Int(self.view.frame.height * 0.5) + 20)
-    
-    coloredSquare.backgroundColor = UIColor.blueColor()
-    coloredSquare.frame = CGRect(x: 0, y: yPosition, width: size, height: size)
-    
-    self.view.addSubview(coloredSquare)
-    
-    UIView.animateWithDuration(1.0, animations: {
-      coloredSquare.backgroundColor = UIColor.redColor()
-      let newFrame = CGRect(x: 0, y: 0, width: size * 2, height: size * 2)
-      coloredSquare.frame = CGRect(x: self.view.frame.width - newFrame.width, y:yPosition, width: newFrame.width, height: newFrame.height)
-      }, completion: { finished in
-        UIView.animateWithDuration(1.0, delay: 1.0, options: .CurveEaseOut, animations: {
-          coloredSquare.backgroundColor = UIColor.blueColor()
-          coloredSquare.frame = CGRect(x: 0, y: yPosition, width: size, height: size)
-          }, completion: {finished in
-            
-            UIView.animateWithDuration(1.0, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: .CurveEaseInOut | .Autoreverse, animations: {
-              coloredSquare.backgroundColor = UIColor.greenColor()
-              coloredSquare.frame = CGRect(x: 0, y: self.view.frame.height - (size * 2), width: size * 2, height: size * 2)
-              }, completion: { finished in
-                // Go back to "home"
-                coloredSquare.backgroundColor = UIColor.blueColor()
-                coloredSquare.frame = CGRect(x: 0, y: yPosition, width: size, height: size)
-                coloredSquare.removeFromSuperview()
-            })
-            
-        })
-    })
+    for loopNumber in 1...10 {
+      let coloredSquare = UIView()
+      let size : CGFloat = CGFloat( Int(rand()) %  40 + 20)
+      let yPosition : CGFloat = CGFloat( Int(rand()) %  Int(self.view.frame.height * 0.5) + 20)
+      
+      coloredSquare.backgroundColor = UIColor.blueColor()
+      coloredSquare.frame = CGRect(x: 0-size, y: yPosition, width: size, height: size)
+      
+      self.view.addSubview(coloredSquare)
+      // randomize the delay
+      let delay = NSTimeInterval(((Float(rand()) % 900.0) + 100.0) / 1000.0)
+      
+      UIView.animateWithDuration(1.0, delay: delay, options: .CurveEaseOut, animations: {
+        coloredSquare.backgroundColor = UIColor.redColor()
+        let newFrame = CGRect(x: 0, y: 0, width: size * 2, height: size * 2)
+        coloredSquare.frame = CGRect(x: self.view.frame.width - newFrame.width, y:yPosition, width: newFrame.width, height: newFrame.height)
+        }, completion: { finished in
+          UIView.animateWithDuration(1.0, delay: delay, options: .CurveEaseOut, animations: {
+            coloredSquare.backgroundColor = UIColor.blueColor()
+            coloredSquare.frame = CGRect(x: 0, y: yPosition, width: size, height: size)
+            }, completion: {finished in
+              
+              UIView.animateWithDuration(1.0, delay: delay * 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: .CurveEaseInOut | .Autoreverse, animations: {
+                coloredSquare.backgroundColor = UIColor.greenColor()
+                coloredSquare.frame = CGRect(x: 0, y: self.view.frame.height - (size * 2), width: size * 2, height: size * 2)
+                }, completion: { finished in
+                  // Go back to "home"
+                  coloredSquare.backgroundColor = UIColor.blueColor()
+                  coloredSquare.frame = CGRect(x: 0, y: yPosition, width: size, height: size)
+                  coloredSquare.removeFromSuperview()
+              })
+              
+          })
+      })
+    }
   }
   
   override func didReceiveMemoryWarning() {

e76f83fc478613711e248da565b5d0ac249b2b9d
kenwilcox <wilcoxk@gmail.com>
2014-12-29 11:26:37 -0700

    Added Push View Controller

---
+++ Animations/PushViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PushViewController.swift
+//  Animations
+//
+//  Created by Kenneth Wilcox on 12/29/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class PushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}

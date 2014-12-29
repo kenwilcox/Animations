af7bc82423d9bebc168ce041eadd91acf0fad2f1
kenwilcox <wilcoxk@gmail.com>
2014-12-29 11:34:20 -0700

    created another green square

--- Animations/PushViewController.swift
+++ Animations/PushViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class PushViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  
+  var greenBox: UIView?
+  var animator: UIDynamicAnimator?
+  var collision: UICollisionBehavior?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.greenBox = UIView(frame: CGRectMake(CGRectGetMidX(self.view.bounds) - 50, 300, 100, 100))
+    self.greenBox!.backgroundColor = UIColor.greenColor()
+    self.greenBox!.transform = CGAffineTransformMakeRotation(CGFloat(M_PI/8))
+    self.view.addSubview(self.greenBox!)
+    
+    self.animator = UIDynamicAnimator(referenceView: self.view)
+    
+    self.collision = UICollisionBehavior(items:[self.greenBox!])
+    self.collision!.translatesReferenceBoundsIntoBoundary = true
+    self.animator!.addBehavior(self.collision!)
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
 }
